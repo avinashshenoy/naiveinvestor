36970a2cf7bd35ef1e90eca7b3b8cfc32014c6aa
Avinash Shenoy <Avinash Shenoy>
2020-02-09 21:10:54 -0800

    Initial Site setup and commit

---
+++ src/pages/about.js
@@ -0,0 +1,43 @@
+import React from "react"
+import {graphql} from "gatsby"
+import Layout from "../components/layout"
+
+export default ({data}) => (
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>
+        Welcome! My name is Avinash. I consider myself an amateur and still naive trader who has been actively dipping his toes in the public markets since 2010. And yet, I feel like i have only scratched the surface of what's available to us.
+    </p>
+    
+    <p>
+        I started out by investing in broad market ETF that tracked major indexes as they were cost effective way to invest while learning the ropes of investing. As more of the market became available to the general public through cheaper commission rates and balance requirement, I started exploring other forms of securities such as direct stock purchase of publicly listed companies to more recently exploring the options markets.
+    </p>
+
+    <p>
+        As is the case with all beginners, I have made quite a few mistakes while learning to trade. Some small while others rather large. I see it as the cost of learning. Lessons learnt by this Naive Investor.
+    </p>
+
+    <p>
+        Sharing one's knowledge and lessons is the best way to remember it. Hence 'I'm dedicating this blog as a space to capture those lessons I learnt through these potentially bad investments as well as share my thoughts on day to day financial events. I hope others find it helpful in their journey to be a a better investor!
+    </p>
+
+    <p>
+      <b>Disclaimer</b>: 
+    </p>
+
+    <p>
+        Ideas and Thoughts expressed in on this site are just that.. <b><i>Thoughts!</i></b> They are in no way intended to be advice or recommendations on securities and/or general stock investments. Trading in securities is inherently risky business. Please consult with your financial advisor on any investment decision you make.
+    </p>
+  
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
---
+++ src/pages/thoughts.md
@@ -0,0 +1,19 @@
+---
+title: "A place for my thoughts"
+date: "2020-02-09"
+---
+
+It's *finally* here! A place for my thoughts on the world of finance. Okay, perhaps not the whole world of finance but my world of finance. I have been mulling starting this space since the middle of last year. Procastination, lazyness, work.. I can make all sort of excuses for why it took this long but that's the past. It's finally deployed.
+
+__Naive Investor__.. Why _Naive_ you may ask? While i was trying to find an appropriate domain name towards the end of last year, my initial few choices where already taken. And then the work Naive popped into my head. One of the definitions that Marriam Websters dictionary defines [Naive](https://www.merriam-webster.com/dictionary/naive) as
+> SELF-TAUGHT\
+> produced by or as if by a self-taught artist
+
+_Nailed it_, it thought to myself. That probably is the best description of who I consider myself to be. I'm a self thought amateur investor. That's me! So _Naive Investor_ it was.
+
+As an amateur investor who is still learning daily about the world of finance, I have made some silly decisions.. a few of them were harmless while others definitely impacted my portfolio. I'm still learning from those mistakes and it takes a bit of work to turn them into muscle memory. 
+
+It occurred to me that some of these learning could be useful to others. And so _Naive Investor_ was born. A space for me to share my learning when it comes to investments and the world of finance. What it won't be a space to recommend investment ideas. I won't pretend to know what is a good investment because i don't, period! 
+
+What I hope to share are interesting facts that cross my eye during my journey in our world of finance. And I hope that others find it interesting as well. 
+
